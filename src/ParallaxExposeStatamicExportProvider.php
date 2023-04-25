8d0c8e68064e19f83fb463ad17f8f2311978cf6c
Garrick3491 <Garrick3491@users.noreply.github.com>
2023-04-25 09:49:41 +0000

    PHP Linting (Pint)

--- src/ParallaxExposeStatamicExportProvider.php
+++ src/ParallaxExposeStatamicExportProvider.php
@@ -2,7 +2,6 @@
 
 namespace Parallax\ExposeStatamicExport\Providers;
 
-use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 use Parallax\ExposeStatamicExport\Statamic\Asset\AssetExists;
 use Statamic\Assets\AssetRepository;
