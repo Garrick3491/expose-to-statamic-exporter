83b0aa873a9824066355a8568398ae13fda1ddba
Simon Garrick <simon.garrick@parall.ax>
2023-04-25 11:06:46 +0100

    add get by slug for entry

--- src/Statamic/Entry/EntryExists.php
+++ src/Statamic/Entry/EntryExists.php
@@ -6,7 +6,7 @@ use Statamic\Facades\Entry;
 
 class EntryExists
 {
-    public function doesEntryExist(string $collection, string $id)
+    public function doesEntryWithIdExist(string $collection, string $id)
     {
         $entry = Entry::query()
             ->where('collection', $collection)
@@ -17,5 +17,18 @@ class EntryExists
         }
 
         return false;
+    }
+
+    public function doesEntryWithSlugExist(string $collection, string $slug)
+    {
+        $entry = Entry::query()
+        ->where('collection', $collection)
+        ->where('slug', $slug);
+
+    if ($entry) {
+        return true;
+    }
+
+    return false;
     }
 }
